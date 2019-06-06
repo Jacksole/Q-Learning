9becde52e291d8ecab01cdfe31b2acda5ec81ecc
Le Aundre Jackson <dell.5287@gmail.com>
2019-06-06 15:20:32 -0400

    Created Files
    Created another example of mountain car with Q table

--- mountiain_car_010.py
+++ mountiain_car_010.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import gym
 
 env = gym.make("MountainCar-v0")
@@ -8,3 +10,8 @@ while not done:
     action = 2
     new_state, reward, done, _ = env.step(action)
     print(reward, new_state)
+    print(env.observation_space.high)
+    print(env.observation_space.low)
+
+q_table = np.random.uniform(
+    low=-2, high=0, size=(DISCRETE_OS_SIZE + [env.action_space.n]))
